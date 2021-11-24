41307e088424be73e7a96079a7a7446cce016b2c
IAM78 <91953698+IAM78@users.noreply.github.com>
2021-11-24 10:17:49 +0330

    Add files via upload

---
+++ fraction.py
@@ -0,0 +1,70 @@
+import math
+
+
+
+
+class kasr :
+    def __init__(self,s=0,m=1):
+        self.sorat=s
+        self.makhraj=m
+    
+
+
+
+    def simple(self):
+        resultsorat=self.sorat
+        resultmakharj=self.makhraj
+
+
+        for i in range(2,10):    
+            if resultsorat%i==0 and resultmakharj%i==0:
+                resultsorat//=i
+                resultmakharj//=i
+                       
+        return(resultsorat,resultmakharj)
+
+
+
+
+    def __add__(self ,other):
+        result=(self.sorat*other.makhraj)+(self.makhraj*other.sorat)
+        return (result,self.makhraj*other.makhraj)
+        
+
+
+
+
+    def __sub__(self ,other):
+        result=(self.sorat*other.makhraj)-(self.makhraj*other.sorat)
+        return (result,self.makhraj*other.makhraj)
+    
+
+
+
+    def __mul__(self , other):
+        return(self.sorat * other.sorat ,self.makhraj * other.makhraj)
+    
+
+
+
+    def __truediv__(self , other):
+        return(self.sorat * other.makhraj ,self.makhraj * other.sorat)
+
+
+
+k1=kasr(8,9)
+k2=kasr(3,4)
+k3=kasr(7,3)
+k4=kasr(11,20)
+
+
+print("Add")
+print(k1+k2)
+print("subtract")
+print(k1-k3)
+print("multiply")
+print(k1*k2)
+print("divide")
+print(k1/k2)
+print("simplification")
+print(k4.simple())
